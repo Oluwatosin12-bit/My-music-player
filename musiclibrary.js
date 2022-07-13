304cdf385afe1533b2db25e7d8669b44fa092a92
Oluwatosin12-bit <85936439+Oluwatosin12-bit@users.noreply.github.com>
2022-07-13 23:10:55 +0100

    Update musiclibrary.js

--- musiclibrary.js
+++ musiclibrary.js
@@ -3,21 +3,21 @@ let trackList = [
     {
         name: "Bless Me",
         path: "music/Bless Me.mp3",
-        img: "music images/music3.jpeg",
+        img: "music3.jpeg",
         artiste: "GoodGirl LA",
     },
 
     {
         name: "Changes",
         path: "music/Changes.mp3",
-        img: "music images/changes.jpg",
+        img: "changes.jpg",
         artiste: "XXXTENTACION",
     },
 
     {
         name: "In My Feelings",
         path: "music/In-My-Feelings.mp3",
-        img: "music images/music5.jpg",
+        img: "music5.jpg",
         artiste: "DRAKE",
     },
 
@@ -25,28 +25,28 @@ let trackList = [
     {
         name: "The Graduation Song",
         path: "music/The Graduation Song.mp3",
-        img: "music images/music4.jpg",
+        img: "music4.jpg",
         artiste: "VITAMIN C",
     },
 
     {
         name: "One By One",
         path: "music/One By One.mp3",
-        img: "music images/music2.jpg",
+        img: "music2.jpg",
         artiste: "ENYA",
     },
 
     {
         name: "Stay With Me",
         path: "music/Stay With Me.mp3",
-        img: "music images/music7.jpg",
+        img: "music7.jpg",
         artiste: "Sam Smith",
     },
 
     {
         name: "Draw Me Close",
         path: "music/Draw Me Close.mp3",
-        img: "music images/music6.jpg",
+        img: "music6.jpg",
         artiste: "Michael .W. Smith",
     },
 ]
